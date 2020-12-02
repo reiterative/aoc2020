92b3f640a9af5ce5b1aac6d26b5ebe6a46f28915
Paul Albertella <reiterative@eiderweb.net>
2020-12-02 21:56:30 +0000

    Solution for day 2

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,57 @@ use std::path::Path;
 // Based on https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
 
 fn main() {
-    day2_1();
+    day2_2();
 }
 
 fn day2_1() {
     if let Ok(lines) = read_lines("./data/input2") {
         let strings = read_strings(lines);
+        let mut valid = 0;
         for s in strings.iter() {
             let words = s.split(' ').collect::<Vec<_>>();
-            let nums = words.get(0).unwrap().split('-');
-            let char = words.get(1).unwrap().strip_suffix(':');
+            let nums = words.get(0).unwrap().split('-').collect::<Vec<_>>();
+            let min = nums.get(0).unwrap().parse::<u32>().unwrap();
+            let max = nums.get(1).unwrap().parse::<u32>().unwrap();
+            let target = words.get(1).unwrap().strip_suffix(':').unwrap();
             let password = words.get(2).unwrap();
-            let x = count_char_in(password, char.unwrap());
+            let matches = password.matches(target).count() as u32;
+            if matches < min || matches > max {
+                println!("Fail: {} - {} is > {} or < {} ", s, matches, max, min);
+            } else {
+                valid = valid + 1;
+            }
         }
+        println!("Total valid: {}", valid)
     }
 }
 
-fn count_char_in(password: &str, char: &str) -> u32 {
-    0
+fn day2_2() {
+    if let Ok(lines) = read_lines("./data/input2") {
+        let strings = read_strings(lines);
+        let mut valid = 0;
+        for s in strings.iter() {
+            let words = s.split(' ').collect::<Vec<_>>();
+            let nums = words.get(0).unwrap().split('-').collect::<Vec<_>>();
+            let first = nums.get(0).unwrap().parse::<usize>().unwrap();
+            let second = nums.get(1).unwrap().parse::<usize>().unwrap();
+            let target = words.get(1).unwrap().strip_suffix(':').unwrap();
+            let password = words.get(2).unwrap();
+            let mut count = 0;
+            if password[first - 1..first] == *target {
+                count = count + 1;
+            }
+            if password[second - 1..second] == *target {
+                count = count + 1;
+            }
+            if count == 1 {
+                valid = valid + 1;
+            } else {
+                println!("Fail: {}", s);
+            }
+        }
+        println!("Total valid: {}", valid)
+    }
 }
 
 fn day1_1() {
@@ -30,7 +63,6 @@ fn day1_1() {
         let numbers = read_numbers(lines);
         find_pair_result(numbers);
     }
-
 }
 
 fn day1_2() {
@@ -38,7 +70,6 @@ fn day1_2() {
         let numbers = read_numbers(lines);
         find_trio_result(numbers);
     }
-
 }
 
 fn find_pair_result(numbers: Vec<i32>) {
@@ -67,7 +98,6 @@ fn find_trio_result(numbers: Vec<i32>) {
     }
 }
 
-
 fn read_numbers(lines: io::Lines<io::BufReader<File>>) -> Vec<i32> {
     // Consumes the iterator, returns an (Optional) String
     let mut numbers = Vec::new();
@@ -92,7 +122,6 @@ fn read_strings(lines: io::Lines<io::BufReader<File>>) -> Vec<String> {
 
     strings
 }
-
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
