dd7945c3cda11e0c20a53cb8e3551ab77983a080
Paul Albertella <reiterative@eiderweb.net>
2020-12-04 23:58:06 +0000

    Working on day 4
    * Part 1 complete
    * Still need to validate hgt, hcl & pid

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,150 @@
 mod utils;
 
 use crate::utils::{read_lines, read_numbers, read_strings};
+use itertools::Itertools;
+use std::collections::HashMap;
 
 fn main() {
-    if let Ok(lines) = read_lines("./data/input3") {
+    if let Ok(lines) = read_lines("./data/input4") {
         let strings = read_strings(lines);
-        //day3_1(&strings);
-        day3_2(&strings, &day3_rules());
+        let valid = day4_1(&strings);
+        println!("Valid passports: {}", valid);
     }
+}
+
+#[cfg(test)]
+mod test4 {
+    use super::*;
+    static TEST_FILE: &str = "./test/test4";
+
+    #[test]
+    fn test4_1() {
+        let valid;
+        if let Ok(lines) = read_lines(TEST_FILE) {
+            let strings = read_strings(lines);
+            valid = day4_1(&strings);
+        } else {
+            panic!("Missing test file: {}", TEST_FILE)
+        }
+        assert_eq!(valid, 2);
+    }
+}
+
+struct PassportEntry {
+    fields: HashMap<String, String>,
+}
+
+impl PassportEntry {
+    fn required_fields() -> Vec<(&'static str, bool)> {
+        let fields = vec![
+            ("byr", false),
+            ("iyr", false),
+            ("eyr", false),
+            ("hgt", false),
+            ("hcl", false),
+            ("ecl", false),
+            ("pid", false),
+            ("cid", true),
+        ];
+        fields
+    }
+
+    fn check_fields(&self) -> bool {
+        for (name, optional) in PassportEntry::required_fields().iter() {
+            if !self.fields.contains_key(*name) {
+                if !optional {
+                    println!("Missing: {}", name);
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    fn validate(&self) -> bool {
+        self.check_byr() && self.check_iyr() && self.check_eyr()
+    }
+
+    fn check_byr(&self) -> bool {
+        self.check_year_range("byr", 1920, 2002)
+    }
+
+    fn check_iyr(&self) -> bool {
+        self.check_year_range("iyr", 2010, 2020)
+    }
+
+    fn check_eyr(&self) -> bool {
+        self.check_year_range("eyr", 2020, 2030)
+    }
+
+    fn check_year_range(&self, field: &str, min: u64, max: u64) -> bool {
+        if self.fields.contains_key(field) {
+            let value = self.fields.get(field).unwrap();
+            let year = check_num(value, 4);
+            match year {
+                Some(y) => return y >= min && y <= max,
+                None => return false,
+            }
+        }
+        false
+    }
+
+    fn check_ecl(&self) -> bool {
+        if self.fields.contains_key("ecl") {
+            let value: &str = self.fields.get("ecl").unwrap();
+            match value {
+                "amb" => return true,
+                "blu" => return true,
+                "brn" => return true,
+                "gry" => return true,
+                "grn" => return true,
+                "hzl" => return true,
+                "oth" => return true,
+                &_ => return false,
+            }
+        }
+        false
+    }
+}
+
+fn check_num(value: &str, required_len: usize) -> Option<u64> {
+    if value.len() == required_len {
+        let num = value.parse::<u64>();
+        if num.is_ok() {
+            return Some(num.unwrap());
+        }
+    }
+    None
+}
+
+fn day4_1(strings: &Vec<String>) -> u32 {
+    let mut valid = 0;
+    let mut entries: Vec<PassportEntry> = Vec::new();
+    let mut fields = HashMap::new();
+    for s in strings {
+        if s.len() == 0 {
+            entries.push(PassportEntry {
+                fields: fields.clone(),
+            });
+            fields.clear();
+        } else {
+            let words = s.split(' ').collect::<Vec<_>>();
+            for w in words {
+                let field_iter = w.split(':');
+                if let Some((field, value)) = field_iter.collect_tuple() {
+                    fields.insert(field.to_string(), value.to_string());
+                }
+            }
+        }
+    }
+
+    for p in entries {
+        if p.check_fields() {
+            valid = valid + 1;
+        }
+    }
+
+    valid
 }
 
 #[cfg(test)]
